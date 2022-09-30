79433c8b3e4de9c7f56dcbd2960c5e60c0c9ab83
Neel Sheth <108276436+neelsheth@users.noreply.github.com>
2022-09-30 16:17:29 +0530

    Update PopupShowCheck.js

--- src/components/PopupShowCheck.js
+++ src/components/PopupShowCheck.js
@@ -17,21 +17,18 @@ export default function PopupShowCheck(props) {
                     </div>
                     <div className='info'>
                         <h3>{props.res.name}</h3>
-                        <div className='text'>language : {props.res.language}</div>
+                        <div className='text'>Language : {props.res.language}</div>
                         {props.res.network !== null && <div className='text'>Channel : {props.res.network.name}</div>}
                         {props.res.network !== null && <div className='text'>Country : {props.res.network.country.name}</div>}
                         {props.res.schedule.time !== "" && <div className='text'>Time : {props.res.schedule.time}</div>}
                         {props.res.rating.average !== null && <div className='text'> Rating : {props.res.rating.average}</div>}
-                        {props.res.status !== null && <div className='text'>status : {props.res.status}</div>}
-                        {props.res.premiered !== null && <div className='text'>premiered : {props.res.premiered}</div>}
-                        {props.res.runtime !== null && <div className='text'>runtime : {props.res.runtime}</div>}
-                        {props.res.genres.length > 0 && <div className='text'>genres : {props.res.genres + "   "}</div>}
+                        {props.res.status !== null && <div className='text'>Status : {props.res.status}</div>}
+                        {props.res.premiered !== null && <div className='text'>Premiered : {props.res.premiered}</div>}
+                        {props.res.runtime !== null && <div className='text'>Runtime : {props.res.runtime}</div>}
+                        {props.res.genres.length > 0 && <div className='text'>Genres : {props.res.genres + "   "}</div>}
                     </div>
-
                 </div>
-
             </div>
-
         </div>
     )
 }
